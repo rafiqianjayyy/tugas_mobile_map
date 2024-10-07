73c18cb65ea1f8ae438dc2a5b4ce555798ab3826
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 12:00:01 +0700

    Create map.dart

---
+++ map.dart
@@ -0,0 +1,37 @@
+//INI ADALAH CARA MEMBUAT MAP
+// void main(){
+//   Map <String, String> nama ={};
+//   var nama1 = Map<String, String>();
+//   var nama2 = <String, String>{};
+
+//   print(nama);
+//   print(nama1);
+//   print(nama2);
+// }
+
+//INI ADALAH CARA MEMANIPULASI MAP
+// void main(){
+//   var namaKu = <String, String>{};
+//   namaKu['awal'] = 'Muhammad';
+//   namaKu['tengah'] = 'Rafiqi';
+//   namaKu['akhir'] = '.';
+
+//   print(namaKu['awal']);
+
+//   namaKu['tengah'] = 'RAFIQI';
+//   print(namaKu);
+
+//   namaKu.remove('akhir');
+//   print(namaKu);  
+// }
+
+//INI ADALAH CONTOH MENDEKLARASIKAN MAP SECARA LANGSUNG
+// void main(){
+//   var nama = {
+//     'awal':'Muhammad',
+//     'tengah':'Rafiqi',
+//     'akhir':'.'
+//   };
+
+//   print(nama['awal']);
+// }
